1eb93b57e144bc33683fd9e36dcf52875aabd892
Neels Hofmeyr <neels@hofmeyr.de>
2016-10-17 01:46:00 +0200

    add jenkins.sh script for jenkins build job
    Change-Id: I6c3d959202f510f0c9780d0fa28c769ea9d721e8

---
+++ contrib/jenkins.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -ex
+
+base="$PWD"
+deps="$base/deps"
+
+mkdir "$deps" || true
+
+cd "$deps"
+osmo-deps.sh libosmocore
+
+cd "$base"
+$MAKE $PARALLEL_MAKE LIBOSMO_DIR="$deps/libosmocore"
