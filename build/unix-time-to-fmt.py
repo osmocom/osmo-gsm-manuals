e2ba56cccee0273165f0b94a90c9ba9b356a0965
Neels Hofmeyr <neels@hofmeyr.de>
2016-10-17 02:18:38 +0200

    portability: use py script instead of 'date -d @1234'
    On FreeBSD, the 'date' command's -d option has a completely different meaning.
    Instead, use a small python script to do the date format conversion, which
    should be more portable.
    
    As a side effect, we now also use UTC instead of the build server's timezone,
    which may be considered a more international choice.
    
    Add build/unix-time-to-fmt.py, call in build/Makefile.asciidoc.inc.
    
    Change-Id: I91a40656184f553ee375216d8ba5c7788fe9990d

---
+++ build/unix-time-to-fmt.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Usage:
+
+  unix-time-to-fmt.py 1234567 [%Y-%m-%d[...]]
+
+Convert unix timestamp to a string of the given format in UTC, according to
+  https://docs.python.org/2/library/time.html
+Default is '%Y-%b-%d' --> 2016-Jan-01
+"""
+
+import sys, time
+
+fmt = '%Y-%b-%d'
+if len(sys.argv) > 2:
+  fmt = sys.argv[2]
+
+print(time.strftime(fmt, time.gmtime(float(sys.argv[1]))))
