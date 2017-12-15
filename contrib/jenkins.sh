ccb51225c51e08a87f75f2491efc1404b99ebc7e
Neels Hofmeyr <neels@hofmeyr.de>
2017-12-15 05:36:49 +0100

    jenkins.sh: incorporate publish step, for master build
    Change-Id: I3c14f4e0266a4eb2702ca9fa9cf05dd54fc5d853

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+publish="$1"
+
 base="$PWD"
 deps="$base/deps"
 export deps
@@ -16,5 +18,11 @@ osmo-deps.sh libosmocore
 cd "$base"
 $MAKE $PARALLEL_MAKE LIBOSMO_DIR="$deps/libosmocore"
 $MAKE $PARALLEL_MAKE LIBOSMO_DIR="$deps/libosmocore" check
+
+if [ "x$publish" = "x--publish" ]; then
+  mkdir out/
+  cp */*.pdf out/
+  rsync -avz --delete -e "ssh -p 48" ./out/ docs@osmocom.org:web-files/latest/
+fi
 
 osmo-clean-workspace.sh
