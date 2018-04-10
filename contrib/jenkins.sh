25df46dda0abe1b85ce27352831528defdfd9210
Alexander Couzens <lynxis@fe80.eu>
2018-04-10 17:26:12 +0200

    jenkins.sh: add hostkey for osmocom.org:48
    Otherwise it will depend on a setting this up in a seperate step.
    This way it's more decoupled from the build host.
    
    Change-Id: Iea1f5810bc7d4370724fdd7eb875c9a27b3d82af

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -22,7 +22,14 @@ $MAKE $PARALLEL_MAKE LIBOSMO_DIR="$deps/libosmocore" check
 if [ "x$publish" = "x--publish" ]; then
   mkdir out/
   cp */*.pdf out/
-  rsync -avz --delete -e "ssh -p 48" ./out/ docs@osmocom.org:web-files/latest/
+
+  cat > "$WORKSPACE/known_hosts" <<EOF
+[osmocom.org]:48 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDgQ9HntlpWNmh953a2Gc8NysKE4orOatVT1wQkyzhARnfYUerRuwyNr1GqMyBKdSI9amYVBXJIOUFcpV81niA7zQRUs66bpIMkE9/rHxBd81SkorEPOIS84W4vm3SZtuNqa+fADcqe88Hcb0ZdTzjKILuwi19gzrQyME2knHY71EOETe9Yow5RD2hTIpB5ecNxI0LUKDq+Ii8HfBvndPBIr0BWYDugckQ3Bocf+yn/tn2/GZieFEyFpBGF/MnLbAAfUKIdeyFRX7ufaiWWz5yKAfEhtziqdAGZaXNaLG6gkpy3EixOAy6ZXuTAk3b3Y0FUmDjhOHllbPmTOcKMry9
+[osmocom.org]:48 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBPdWn1kEousXuKsZ+qJEZTt/NSeASxCrUfNDW3LWtH+d8Ust7ZuKp/vuyG+5pe5pwpPOgFu7TjN+0lVjYJVXH54=
+[osmocom.org]:48 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK8iivY70EiR5NiGChV39gRLjNpC8lvu1ZdHtdMw2zuX
+EOF
+  SSH_COMMAND="ssh -o 'UserKnownHostsFile=$WORKSPACE/known_hosts' -p 48"
+  rsync -avz --delete -e "$SSH_COMMAND" ./out/ docs@osmocom.org:web-files/latest/
 fi
 
 osmo-clean-workspace.sh
