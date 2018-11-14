82c6c9ba143e192debd52bc605d46de3e58c22a0
Oliver Smith <osmith@sysmocom.de>
2018-11-14 10:59:25 +0100

    jenkins.sh: do not use libosomcore source
    Build scripts do not depend on the libosmocore sources anymore
    with 161365f ("merge_doc.xsl: move from libosmocore.git"),
    so don't try to use them in jenkins.sh anymore.
    
    Related: OS#3385
    Change-Id: Ied3661b8a02e9defca0e5aedcc4494845553db84

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -4,20 +4,10 @@ set -ex
 
 publish="$1"
 
-base="$PWD"
-deps="$base/deps"
-export deps
-
 osmo-clean-workspace.sh
 
-mkdir "$deps" || true
-
-cd "$deps"
-osmo-deps.sh libosmocore
-
-cd "$base"
-$MAKE $PARALLEL_MAKE LIBOSMO_DIR="$deps/libosmocore"
-$MAKE $PARALLEL_MAKE LIBOSMO_DIR="$deps/libosmocore" check
+$MAKE $PARALLEL_MAKE
+$MAKE $PARALLEL_MAKE check
 
 if [ "x$publish" = "x--publish" ]; then
   mkdir out/
