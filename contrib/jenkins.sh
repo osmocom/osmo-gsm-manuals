e7446e88104796a76bc74929b21e320ddb3d4a62
Oliver Smith <osmith@sysmocom.de>
2018-11-14 11:09:31 +0100

    publish from project repos, not this repo anymore
    Do not publish PDFs for all projects anymore with jenkins.sh --publish
    or "make publish". Extract known_hosts from jenkins.sh, and install it
    along with the other shared files in OSMO_GSM_MANUALS_DIR. Add a
    "publish" target to Makefile.common.inc, so we can use it from the
    project repositories. Document its usage in INSTALL.txt.
    
    No automatism for building and publishing the documentation of all
    projects will be implemented in this patch series, as discussed here:
    https://osmocom.org/issues/3385#note-7
    
    (moving manuals to project repositories 19/19)
    
    Related: OS#3385
    Change-Id: Ibe0424ceace151115985896b6d43035c69067c50

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -2,7 +2,12 @@
 
 set -ex
 
-publish="$1"
+if [ -n "$1" ]; then
+    echo "ERROR: --publish has been deprecated!"
+    echo "Manuals have been moved to the projects, and need to be published from there."
+    echo "See INSTALL.txt for more information."
+    exit 1
+fi
 
 osmo-clean-workspace.sh
 
@@ -11,18 +16,5 @@ autoreconf -fi
 $MAKE $PARALLEL_MAKE
 $MAKE $PARALLEL_MAKE check
 $MAKE $PARALLEL_MAKE distcheck
-
-if [ "x$publish" = "x--publish" ]; then
-  mkdir out/
-  cp */*.pdf out/
-
-  cat > "$WORKSPACE/known_hosts" <<EOF
-[rita.osmocom.org]:48 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDgQ9HntlpWNmh953a2Gc8NysKE4orOatVT1wQkyzhARnfYUerRuwyNr1GqMyBKdSI9amYVBXJIOUFcpV81niA7zQRUs66bpIMkE9/rHxBd81SkorEPOIS84W4vm3SZtuNqa+fADcqe88Hcb0ZdTzjKILuwi19gzrQyME2knHY71EOETe9Yow5RD2hTIpB5ecNxI0LUKDq+Ii8HfBvndPBIr0BWYDugckQ3Bocf+yn/tn2/GZieFEyFpBGF/MnLbAAfUKIdeyFRX7ufaiWWz5yKAfEhtziqdAGZaXNaLG6gkpy3EixOAy6ZXuTAk3b3Y0FUmDjhOHllbPmTOcKMry9
-[rita.osmocom.org]:48 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBPdWn1kEousXuKsZ+qJEZTt/NSeASxCrUfNDW3LWtH+d8Ust7ZuKp/vuyG+5pe5pwpPOgFu7TjN+0lVjYJVXH54=
-[rita.osmocom.org]:48 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK8iivY70EiR5NiGChV39gRLjNpC8lvu1ZdHtdMw2zuX
-EOF
-  SSH_COMMAND="ssh -o 'UserKnownHostsFile=$WORKSPACE/known_hosts' -p 48"
-  rsync -avz --delete -e "$SSH_COMMAND" ./out/ docs@rita.osmocom.org:web-files/latest/
-fi
 
 osmo-clean-workspace.sh
