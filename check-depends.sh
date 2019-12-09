acc127c02298812bf3b933db27914c139e111113
Oliver Smith <osmith@sysmocom.de>
2019-12-09 08:38:35 +0100

    debian/check-depends.sh: add inkscape to depends
    Related: OS#4314
    Change-Id: I83864af60ccb1ae4170c2cec90ce3f388cc305ae

--- check-depends.sh
+++ check-depends.sh
@@ -15,5 +15,6 @@ check_dep_bin asciidoc asciidoc
 check_dep_bin dblatex dblatex
 check_dep_bin packetdiag3 python3-nwdiag
 check_dep_bin dot graphviz
+check_dep_bin inkscape inkscape
 
 echo "All dependencies installed!"
